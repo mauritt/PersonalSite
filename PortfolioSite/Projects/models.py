abf46bc8465bedaaf1a28723ee26c60a2a8d8f01
Michael Auritt <michael@michaelauritt.com>
2017-09-21 15:30:15 -0400

    Added initail models for Projects and Project Hosts

--- PortfolioSite/Projects/models.py
+++ PortfolioSite/Projects/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Host(models.Model):
+    name = models.CharField(max_length=20)
+    base_site_URL = models.URLField()
+    base_embed_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Project_type(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = ("Project Type")
+
+
+class Project(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    hidden = models.BooleanField(default = False)
+    priority = models.IntegerField()
+    url = models.URLField()
+    host_code = models.CharField(max_length = 15, blank = True, null=True)
+    host = models.ForeignKey('Host', blank=True, null=True)
+    thumbnail_image = models.ImageField(upload_to='uploads/thumbnails')
+    project_type = models.ForeignKey('Project_type')
+
+
+    def __str__(self):
+        return self.name
