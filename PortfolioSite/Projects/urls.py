2f240a68d88e2c21a2b86a9ef7f5dd7a41288c95
Michael Auritt <michael@michaelauritt.com>
2017-09-26 16:48:14 -0400

    Updated URLs and views to choose list or tiled view

--- PortfolioSite/Projects/urls.py
+++ PortfolioSite/Projects/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    url(r'^portfolio/(?P<project_type>[A-z]+)/$', views.portfolio, name='portfolio'),
+    url(r'^portfolio/(?P<project_type>[A-z]+)/(?P<view>(Tile|List|tile|list))$', views.portfolio, name='portfolio'),
     url(r'^portfolio/detail/(?P<project_id>[A-z 0-9]+)/$', views.detail, name='detail'),
 ]
--- PortfolioSite/Projects/views.py
+++ PortfolioSite/Projects/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponse
 from .models import Project, Project_type, Host
 
 
-def portfolio(request, project_type):
+def portfolio(request, project_type, view):
     proj_type_id = get_object_or_404(Project_type, name__iexact = project_type)
     examples = Project.objects.all().filter(project_type = proj_type_id.id)
     context = {'project_type':project_type,'examples':examples}
 
-    if project_type.lower() == 'video':
-        return render(request, 'portfolio/videoPortfolio.html', context)
+    if view.lower() == 'tile':
+        return render(request, 'portfolio/tiledPortfolio.html', context)
 
-    if project_type.lower() == 'code':
+    if view.lower() == 'list':
         return render(request, 'portfolio/listPortfolio.html', context)
 
 def detail(request, project_id):
