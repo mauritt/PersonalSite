64316f838f0cd97d603a442077057faf9566e145
Michael Auritt <michael@michaelauritt.com>
2017-10-04 14:43:23 -0400

    Updated landing page layout per redesign

--- PortfolioSite/static/js/portfolio.js
+++ PortfolioSite/static/js/portfolio.js
@@ -1,30 +1,11 @@
-function show_project(obj, proj_id){
-    screen = document.getElementById("screen");
-    screen.style.visibility ="visible";
-    screen.style.opacity = ".90";
-    lightbox = document.getElementById("lightbox");
 
-    $(lightbox).load("/portfolio/detail/" + proj_id + " #details");
+function changePage(obj, section){
+    var main = getElementById('Main');
+    var newPage = obj.innerHTML;
+    var url = './' + section + '/' + newPage;
 
-    lightbox.style.visibility="visible";
-
-    lightbox.style.opacity = "1";
-};
-
-function hide_video(){
-    var screen = document.getElementById('screen');
-    screen.style.opacity = "0"
-    screen.style.visibility = "hidden";
-    var x = document.getElementById('closeButton');
-    x.style.opacity = "0"
-    x.style.visibility = "hidden";
-    var lightbox = document.getElementById("lightbox");
-    var embed = document.getElementById("details");
-    lightbox.style.opacity = "0"
-    lightbox.style.visibility="hidden";
-    lightbox.removeChild(embed);
-};
-
+    $(main).load(url + " #project")
+}
 
 function swapLogo(obj,imagePath){
     var newPath = obj.src.split('/');
@@ -48,26 +29,4 @@ function activatePage(){
 
     };
 };
-
-
-window.onload = function(){
-
-    activatePage();
-
-    var projects = document.getElementsByClassName("project");
-
-    function time_delay(i){
-        var delay=200;
-        return setTimeout(function(){
-            projects[i].style.display = "inline";
-
-        }, delay*i);
-    };
-
-    for(i=0;i<projects.length;i++){
-        time_delay(i);
-
-    };
-};
-
 
